90818aceff703936970d5e3136ad4e27e8ca1758
Rachel Spicer <rachelaspicer@gmail.com>
2021-09-22 11:16:45 +0100

    add cluster comparison output

--- 05_cluster_comparison/cluster_comparison.r
+++ 05_cluster_comparison/cluster_comparison.r
@@ -26,105 +26,12 @@ C2.1vC2.2 <- compare_clusters(metadata, cluster1 = c("2.1.1", "2.1.2"), cluster2
 # Cluster 2.1.1 vs Cluster 2.1.2 
 C2.1.1vC2.1.2 <- compare_clusters(metadata, cluster1 = "2.1.1", cluster2 = "2.1.2", name_cluster1 = "C2.1.1", name_cluster2 = "C2.1.2")
 
-test <- C1vC2 %>%
-  group_by(`Question ID`) %>%
-  mutate(max_diff = max(Difference)) %>%
-  filter(max_diff >= 55)
+# Create output directory
+make.dir("./output")
 
-
-test <- C1.1vC1.2 %>%
-  group_by(`Question ID`) %>%
-  mutate(max_diff = max(Difference)) %>%
-  filter(max_diff >= 50)
-
-test <- C2.1vC2.2 %>%
-  group_by(`Question ID`) %>%
-  mutate(max_diff = max(Difference)) %>%
-  filter(max_diff >= 50)
-
-# By sub-cluster
-# Cluster 2.1 vs. C1 & C2.2
-C2.1vC1C2.2 <- metadata %>%
-  mutate(Cluster = case_when(Cluster == "2.1.1" | Cluster == "2.1.2" ~ "C2.1",
-                             Cluster == "1.1" | Cluster == "1.2" | Cluster == "2.2" ~ "C1&C2.2")) %>%
-  mutate(Cluster = as.factor(as.character(Cluster))) %>%
-  select(-ID, -`Entry ID`, -`Branching question`, -`Entry name`, -`Entry source`, -`Entry description`, -`Entry tags`, -Expert, -`Region ID`, -`Region name`, -`Region description`, -`Region tags`, -elite, -non_elite, -religious_specialist) %>%
-  mutate_all(as.character) %>%
-  pivot_longer(c(-Cluster), names_to = "Question", values_to = "Answers") %>%
-  group_by(Cluster, Question, Answers) %>%
-  summarise(Frequency = n()) %>%
-  ungroup() %>%
-  group_by(Cluster, Question) %>%
-  mutate(group_total = sum(Frequency)) %>%
-  group_by(Cluster, Question, Answers) %>%
-  mutate(Percentage = case_when(Cluster == "C2.1" ~ Frequency/group_total * 100,
-                                Cluster == "C1&C2.2" ~ Frequency/group_total * 100)) %>%
-  mutate(Percentage = round(Percentage, 2)) %>%
-  select(-Frequency, -group_total) %>%
-  pivot_wider(names_from = Cluster, values_from = Percentage) %>%
-  mutate(C2.1 = ifelse(is.na(C2.1), 0, C2.1)) %>%
-  mutate(`C1&C2.2` = ifelse(is.na(`C1&C2.2`), 0, `C1&C2.2`)) %>%
-  mutate(Difference = abs(C2.1 - `C1&C2.2`)) %>%
-  rename("Question ID" = "Question") %>%
-  mutate(`Question ID` = as.numeric(`Question ID`)) %>%
-  inner_join(questions) %>%
-  select(`Question ID`, Question, everything()) %>%
-  arrange(desc(Difference))
-
-# Cluster 2.2 vs. C1 & C2.1
-C2.2vC1C2.1 <- metadata %>%
-  mutate(Cluster = case_when(Cluster == "2.2" ~ "C2.2",
-                             Cluster == "1.1" | Cluster == "1.2" | Cluster == "2.1.1" | Cluster == "2.1.2" ~ "C1&C2.1")) %>%
-  mutate(Cluster = as.factor(as.character(Cluster))) %>%
-  select(-ID, -`Entry ID`, -`Branching question`, -`Entry name`, -`Entry source`, -`Entry description`, -`Entry tags`, -Expert, -`Region ID`, -`Region name`, -`Region description`, -`Region tags`, -elite, -non_elite, -religious_specialist) %>%
-  mutate_all(as.character) %>%
-  pivot_longer(c(-Cluster), names_to = "Question", values_to = "Answers") %>%
-  group_by(Cluster, Question, Answers) %>%
-  summarise(Frequency = n()) %>%
-  ungroup() %>%
-  group_by(Cluster, Question) %>%
-  mutate(group_total = sum(Frequency)) %>%
-  group_by(Cluster, Question, Answers) %>%
-  mutate(Percentage = case_when(Cluster == "C2.2" ~ Frequency/group_total * 100,
-                                Cluster == "C1&C2.1" ~ Frequency/group_total * 100)) %>%
-  mutate(Percentage = round(Percentage, 2)) %>%
-  select(-Frequency, -group_total) %>%
-  pivot_wider(names_from = Cluster, values_from = Percentage) %>%
-  mutate(C2.2 = ifelse(is.na(C2.2), 0, C2.2)) %>%
-  mutate(`C1&C2.1` = ifelse(is.na(`C1&C2.1`), 0, `C1&C2.1`)) %>%
-  mutate(Difference = abs(C2.2 - `C1&C2.1`)) %>%
-  rename("Question ID" = "Question") %>%
-  mutate(`Question ID` = as.numeric(`Question ID`)) %>%
-  inner_join(questions) %>%
-  select(`Question ID`, Question, everything()) %>%
-  arrange(desc(Difference))
-
-# C1 vs. C2.1
-C1vC2.1 <- metadata %>%
-  filter(Cluster != "2.2") %>%
-  mutate(Cluster = case_when(Cluster == "1.1" | Cluster == "1.2" ~ "C1",
-                             Cluster == "2.1.1" | Cluster == "2.1.2" ~ "C2.1")) %>%
-  mutate(Cluster = as.factor(as.character(Cluster))) %>%
-  select(-ID, -`Entry ID`, -`Branching question`, -`Entry name`, -`Entry source`, -`Entry description`, -`Entry tags`, -Expert, -`Region ID`, -`Region name`, -`Region description`, -`Region tags`, -elite, -non_elite, -religious_specialist) %>%
-  mutate_all(as.character) %>%
-  pivot_longer(c(-Cluster), names_to = "Question", values_to = "Answers") %>%
-  group_by(Cluster, Question, Answers) %>%
-  summarise(Frequency = n()) %>%
-  ungroup() %>%
-  group_by(Cluster, Question) %>%
-  mutate(group_total = sum(Frequency)) %>%
-  group_by(Cluster, Question, Answers) %>%
-  mutate(Percentage = case_when(Cluster == "C1" ~ Frequency/group_total * 100,
-                                Cluster == "C2.1" ~ Frequency/group_total * 100)) %>%
-  mutate(Percentage = round(Percentage, 2)) %>%
-  select(-Frequency, -group_total) %>%
-  pivot_wider(names_from = Cluster, values_from = Percentage) %>%
-  mutate(C1 = ifelse(is.na(C1), 0, C1)) %>%
-  mutate(C2.1 = ifelse(is.na(C2.1), 0, C2.1)) %>%
-  mutate(Difference = abs(C1 - C2.1)) %>%
-  rename("Question ID" = "Question") %>%
-  mutate(`Question ID` = as.numeric(`Question ID`)) %>%
-  inner_join(questions) %>%
-  select(`Question ID`, Question, everything()) %>%
-  arrange(desc(Difference))
+# Save comparisons
+write_csv(C1vC2, "./output/c1vc2.csv")
+write_csv(C1.1vC1.2, "./output/c1.1vc1.2.csv")
+write_csv(C2.1vC2.2, "./output/c2.1vc2.2.csv")
+write_csv(C2.1.1vC2.1.2, "./output/c2.1.1vc2.1.2.csv")
 
